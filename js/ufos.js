6203ed81159b16f2612cb36b36e0df7984b49fe2
Robert Hasegawa <rob5704@gmail.com>
2018-01-26 13:44:43 +1100

    basic ufo code, now spawns on random y, and moves to the left

--- js/ufos.js
+++ js/ufos.js
@@ -3,19 +3,32 @@ var ufosCanvas = document.querySelector("#ufos");
 var ctxU = ufosCanvas.getContext("2d");
 ufosCanvas.width = 400;
 ufosCanvas.height = 500;
+var raf;
 
-var ufos = [];
+function getRandomInt (min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; }
 
-function Ufo(){
-  this.x = 400;
-  this.y = getRandomInt(0,400);
-  this.size = 10;
-  this.fill
-}
+var ufo = {
+  x: 400,
+  y: getRandomInt(0,500),
+  vx: -1,
+  vy: 0,
+  radius: 1,
+  color:'red',
 
-function drawUfo(){
-  for (var i = 0; i <ufos.length; i++){
-    var u = ufos[i];
-    ctxU.fillRect()
+  draw: function(){
+    ctxU.beginPath();
+    ctxU.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctxU.closePath();
+    ctxU.fillStyle = this.color;
+    ctxU.fill();
   }
 }
+function draw(){
+
+  ctxU.clearRect(0,0,ufosCanvas.width,ufosCanvas.height);
+  ufo.draw();
+  ufo.x += ufo.vx;
+  requestAnimationFrame(draw);
+
+}
+requestAnimationFrame(draw);
