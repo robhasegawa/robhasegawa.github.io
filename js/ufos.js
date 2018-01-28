289499a660fe27aa9d3e11189c7ac9a64484b367
Robert Hasegawa <rob5704@gmail.com>
2018-01-28 12:39:12 +1100

    added in gradient

--- js/ufos.js
+++ js/ufos.js
@@ -12,10 +12,9 @@ var ufo = {
   vx: -1 *(Math.random() * 2 +.1),
   vy: 0,
   radius: 1,
-  color:'red',
+  color:'black',
 
   draw: function(){
-
     ctxU.beginPath();
     ctxU.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
     ctxU.closePath();
@@ -23,26 +22,39 @@ var ufo = {
     ctxU.fill();
   }
 }
+
+function blinker(){
+  setTimeout(function(){flashRed;},40);
+}
+function flashRed(){
+  var grd = ctxU.createRadialGradient(ufo.x,ufo.y,1,ufo.x,ufo.y,5);
+  grd.addColorStop(0,'rgba(256,0,0,1)');
+  grd.addColorStop(1,'rgba(256,256,256,0)');
+  ctxU.fillStyle = grd;
+  ctxU.fillRect(ufo.x-5,ufo.y-5,10 ,10);
+
+}
+
 function draw(){
 
   ctxU.clearRect(0,0,ufosCanvas.width,ufosCanvas.height);
   ufo.draw();
-
   ufo.x += ufo.vx;
+  setTimeout(function(){requestAnimationFrame(flashRed);},40);
 
     if (ufo.x <= 0){
         ctxU.clearRect(0,0,ufosCanvas.width,ufosCanvas.height);
         setTimeout(function(){
         requestAnimationFrame(draw);
       },getRandomInt(3000,8000));
-       ufo.x = 400;
+       ufo.x = 405;
        ufo.y = getRandomInt(0,500);
-       ufo.vx = -1 *(Math.random() * 2 +.1);
+       ufo.vx = -1 *(Math.random() * 1 +.1);
       }
 else {
   requestAnimationFrame(draw);
-  console.log(ufo.vx)
+}
+}
+blinker();
 
-}
-}
 requestAnimationFrame(draw);
