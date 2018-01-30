758aca9916355ed3b2fd108d703eb9859ce2f883
Robert Hasegawa <rob5704@gmail.com>
2018-01-30 12:32:50 +1100

    Added flashing

--- js/ufos.js
+++ js/ufos.js
@@ -10,8 +10,7 @@ var ufo = {
   x: 400,
   y: getRandomInt(0,500),
   vx: -1 *(Math.random() * 2 +.1),
-  vy: 0,
-  radius: 1,
+  radius: .5,
   color:'black',
 
   draw: function(){
@@ -23,38 +22,59 @@ var ufo = {
   }
 }
 
-function blinker(){
-  setTimeout(function(){flashRed;},40);
-}
 function flashRed(){
   var grd = ctxU.createRadialGradient(ufo.x,ufo.y,1,ufo.x,ufo.y,5);
   grd.addColorStop(0,'rgba(256,0,0,1)');
   grd.addColorStop(1,'rgba(256,256,256,0)');
   ctxU.fillStyle = grd;
   ctxU.fillRect(ufo.x-5,ufo.y-5,10 ,10);
+  setTimeout(function(){
+    requestAnimationFrame(flashInterval);
+    },100);
 
+}
+
+function flashGreen(){
+  var grd = ctxU.createRadialGradient(ufo.x,ufo.y,1,ufo.x,ufo.y,5);
+  grd.addColorStop(0,'rgba(0,256,0,1)');
+  grd.addColorStop(1,'rgba(256,256,256,0)');
+  ctxU.fillStyle = grd;
+  ctxU.fillRect(ufo.x-5,ufo.y-5,10 ,10);
+  setTimeout(function(){
+    requestAnimationFrame(flashInterval2);
+    },2000);
+}
+
+function flashInterval(){
+  ctxU.clearRect(ufo.x-5,ufo.y-5,10 ,10);
+  setTimeout(function(){
+    requestAnimationFrame(flashGreen);
+    },10);
+}
+
+function flashInterval2(){
+  ctxU.clearRect(ufo.x-5,ufo.y-5,10 ,10);
+  setTimeout(function(){
+    requestAnimationFrame(flashRed);
+    },1000);
 }
 
 function draw(){
-
   ctxU.clearRect(0,0,ufosCanvas.width,ufosCanvas.height);
   ufo.draw();
   ufo.x += ufo.vx;
-  setTimeout(function(){requestAnimationFrame(flashRed);},40);
-
-    if (ufo.x <= 0){
+  if (ufo.x <= 0){
         ctxU.clearRect(0,0,ufosCanvas.width,ufosCanvas.height);
         setTimeout(function(){
+            requestAnimationFrame(draw);
+            },getRandomInt(3000,8000));
+        ufo.x = 405;
+        ufo.y = getRandomInt(0,500);
+        ufo.vx = -1 *(Math.random() * 1 +.1);
+        }
+  else {
         requestAnimationFrame(draw);
-      },getRandomInt(3000,8000));
-       ufo.x = 405;
-       ufo.y = getRandomInt(0,500);
-       ufo.vx = -1 *(Math.random() * 1 +.1);
-      }
-else {
-  requestAnimationFrame(draw);
+  }
 }
-}
-blinker();
-
+requestAnimationFrame(flashRed);
 requestAnimationFrame(draw);
